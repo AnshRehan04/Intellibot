b720a06855e40766b6205cc7d2895f88b4891084
Ansh Rehan <118544177+AnshRehan04@users.noreply.github.com>
2025-07-14 23:00:25 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
     setSuccess(false);
     
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('https://intellibot-rswr.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
